e0e4e98a1f648a1767cdf16a568d94c816a4db58
Truedick23 <dingzhx@vip.qq.com>
2019-03-04 22:47:05 +0800

    1

--- RenameCertainPerformer.py
+++ RenameCertainPerformer.py
@@ -1,25 +1,29 @@
 import os
 import BasicSongsOperator as bso
-dir = "E:/CloudMusic/"
-performerList = ['The Killers', 'Queen', 'The Flying Burrito Brothers', 'Scissor Sisters', 'New York Dolls', 'Elton John',
-                 'Björk', 'Albert King', 'Sonny Boy Williamson', "Flamin' Groovies", 'Moby Grape', 'X', 'The Smiths',
-                 'Patti Smith', 'Sleater-Kinney', 'Bronski Beat', 'Tori Amos', 'Yazoo', 'Erasure', 'Danse Society',
-                 'Danse Society', 'New Order', 'The Sound']
-for performer in performerList:
-    performerDir = dir + performer + '/'
-    albumList = os.listdir(performerDir)
-    for album in albumList:
-        albumDir = performerDir + album + '/'
-        print(albumDir)
-        os.chdir(albumDir)
-        songsList = os.listdir(albumDir)
-        for song in songsList:
-            srcFile = albumDir + song
-            try:
-                trackNum, performer, songName, _ = bso.acquireInfo(srcFile)
-                newName = trackNum + ' - ' + songName + ' - ' + performer + '.mp3'
-                if newName != song:
-                    os.rename(song, newName)
-                    print(newName, song)
-            except:
-                continue
+import sys
+
+def rename_performers(dir, performerList):
+    for performer in performerList:
+        performerDir = dir + performer + '/'
+        albumList = os.listdir(performerDir)
+        for album in albumList:
+            albumDir = performerDir + album + '/'
+            print(albumDir)
+            os.chdir(albumDir)
+            songsList = os.listdir(albumDir)
+            for song in songsList:
+                srcFile = albumDir + song
+                try:
+                    trackNum, performer, songName, _ = bso.acquireInfo(srcFile)
+                    newName = trackNum + ' - ' + songName + ' - ' + performer + '.mp3'
+                    if newName != song:
+                        os.rename(song, newName)
+                        print(newName, song)
+                except:
+                    continue
+
+if __name__ == '__main__':
+    dir = "E:/CloudMusic/"
+    performerList = ['Victor Wooten', 'Kraftwerk', 'Radiohead', 'The Yardbirds', 'Marcus Miller', 'Jaco Pastorius']
+    rename_performers(dir, performerList)
+
